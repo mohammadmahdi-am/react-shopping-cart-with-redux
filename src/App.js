1627ac9690f73a03b3845932393e82efc9616073
MohammadMahdi <mamiriesfahani@gmail.com>
2021-03-24 18:14:40 +0430

    Revert "filter products"
    This reverts commit cde096725d2d69f607d2d9f66d98947e261732aa.

--- src/App.js
+++ src/App.js
@@ -1,72 +1,17 @@
 import { useState } from "react";
-import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json";
 function App() {
   const [products, setProducts] = useState(data.mobiles);
-  const [filter,setFilter] = useState("")
-  const [sort,setSort] = useState("")
-
-  const sortProducts = (e) => {
-    switch(e.target.value){
-      case "Lowest":
-        setSort("Lowest")
-
-        setProducts(products.slice().sort(function(a, b) {
-          return a.price - b.price;
-        }))
-        break
-
-      
-      
-      case "Highest":
-        setSort("Highest")
-        setProducts(products.slice().sort(function(a, b) {
-          return b.price - a.price;
-        }))
-        break
-
-      case "Name":
-        setSort("Name")
-        
-        setProducts(products.slice().sort((a, b) => a.model.localeCompare(b.model)))
-        break
-
-      default : setSort("")
-    }
-    setSort(e.target.value)
-
-  }
-
-  const filterProducts = (e) => {
-    
-    if(e.target.value === ""){
-     
-      setFilter(e.target.value)
-      setProducts(data.mobiles)
-
-    }else{
-      let productsCopy = data.mobiles.slice()
-      productsCopy = productsCopy.filter((item)=>item.availableColors.indexOf(e.target.value) >= 0)
-      setFilter(e.target.value)
-      setProducts(productsCopy)
-    
-    
-    }
-      
-
-
-  }
 
   return (
     <div className="App">
       <header className="container-fluid bg-primary text-white py-2">
-        <h3>Mobiloe shop</h3>
+        <h3>Mobile shop</h3>
       </header>
       <div className="container-fluid">
         <div className="row flex-column flex-md-row">
           <main className="col-md-10 col-12 order-2 order-md-1">
-            <Filter filterProducts={filterProducts} filter={filter} sort={sort} sortProducts={sortProducts}/>
             <Products products={products} />
           </main>
           <aside className="col-md-2 col-12 order-1 order-md-2 my-5 text-center">SideBar</aside>
--- src/components/Products.js
+++ src/components/Products.js
@@ -3,16 +3,14 @@ import React from "react";
 function Products(props) {
   return (
     <div className="row">
-      {props.products.map((product,index) => (
-        <div className="col-12 col-md-4 text-center my-5" key={index}>
+      {props.products.map((product) => (
+        <div className="col-12 col-md-4 text-center my-5">
           <img
             src={product.image}
             className="d-inline-block w-50 h-50 "
             alt={product.model}
           />
           <h4 className="text-center">{product.model}</h4>
-          <h5>Available Colors </h5>
-          <div className="d-flex justify-content-center">{product.availableColors.map((color,key)=>(<span key={key} className="mx-2 rounded-circle" style={{backgroundColor:color,width:"30px",height:"30px",display:"inline-block",border:"1px solid rgba(0,0,0,0.5)"}}></span>))}</div>
           <h4 className="text-center">{product.price} Toman</h4>
           <div className="btn-group">
             <a href="" className="btn btn-primary">
